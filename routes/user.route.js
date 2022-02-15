2fc5c5637742be86c69a3ed8350a280fa32ccba4
Avijit Pandey <avijeetpandey87@gmail.com>
2022-02-15 14:31:50 +0530

    route: user: add create and read routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,8 @@
-//import the controller defined in ../controller and use it here
-//then export the router.
 const router = require('express').Router()
 
 const auth = require('../middlewares/auth.middleware')
-const controller = require('../controllers/example.controller')
+const controller = require('../controllers/user.controller')
 
-router.get('/example', auth(), controller.read)
+router.post('/user', auth(), controller.add)
 
 module.exports = router
