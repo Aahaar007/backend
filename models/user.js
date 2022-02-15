79abf88cf550489743b2898395a363bd73127bcb
Avijit Pandey <avijeetpandey87@gmail.com>
2022-02-15 10:24:55 +0530

    model: update user schema

--- models/user.js
+++ models/user.js
@@ -1,72 +1,65 @@
-const mongoose = require("mongoose");
-const Joi = require('joi');
-const _ = require('lodash');
+const mongoose = require('mongoose')
+const _ = require('lodash')
 
 const userSchema = new mongoose.Schema({
-    uid:{
-        type: String,
-    },
-    name: {
-        type:String,
-        required: true,
-        minlength : 4,
-        maxlength: 50,
-        trim: true,
-    },
-    phone: {
-       type: Number,
-       required: true,
-       minlength: 9,
-       maxlength: 9,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 4,
-        maxlength: 1024,
-    },
-    food: {
-       listed:[{
-           id: {
-               type: mongoose.Schema.Types.ObjectId,
-               ref: 'FoodOrder',
-               required: true
-           }
-       }],
-       donated: [{
-           id:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'FoodOrder',
-            required: true 
-           }
-       }],
-       recieved: [{
-        id:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'FoodOrder',
-            required: true 
-           }
-       }]
-    },
-    
-    address: {
-        type: String,
-        required: true,
-     },
+  uid: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 50,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    minlength: 9,
+    maxlength: 9,
+  },
+  food: {
+    listed: [
+      {
+        id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'FoodOrder',
+          required: true,
+        },
+      },
+    ],
+    donated: [
+      {
+        id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'FoodOrder',
+          required: true,
+        },
+      },
+    ],
+    recieved: [
+      {
+        id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'FoodOrder',
+          required: true,
+        },
+      },
+    ],
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+})
 
-     isVerified :{
-         type: Boolean,
-         default: false,
-         required: true,
-     },
+const User = mongoose.model('User', userSchema)
 
-     onboardingStatus: {
-         type: Boolean,
-         default: false,
-     }
-
-});
-
-const User= mongoose.model('User', userSchema);
-
-exports.User = User;
+exports.User = User
