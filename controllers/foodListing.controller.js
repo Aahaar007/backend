3d27561b36960820df665cdd4fa3c17d2c932c85
Avijit Pandey <avijeetpandey87@gmail.com>
2022-04-08 16:21:51 +0530

    route: foodListing: modify multer ot use s3, send presigned url when fetching objects.

--- controllers/foodListing.controller.js
+++ controllers/foodListing.controller.js
@@ -1,10 +1,11 @@
 const { FoodListing, validateCreate } = require('../models/foodListing.model')
+const preSigner = require('../utility/urlGenerator')
 
 const add = async (req, res) => {
   const { error } = validateCreate(req.body)
   if (error) return res.status(400).send({ error: error.message })
   try {
-    const foodListing = new FoodListing({
+    let foodListing = new FoodListing({
       donorId: req.uid,
       ...req.body,
       photos: req.files['refImage']
@@ -12,6 +13,8 @@ const add = async (req, res) => {
         : [],
     })
     await foodListing.save()
+    foodListing = foodListing.toObject()
+    foodListing.photos = await preSigner(foodListing.photos)
     return res.status(200).send({
       message: 'Food listing created successfully.',
       foodListing,
--- routes/foodListing.route.js
+++ routes/foodListing.route.js
@@ -2,23 +2,35 @@ const router = require('express').Router()
 
 const auth = require('../middlewares/auth.middleware')
 const controller = require('../controllers/foodListing.controller')
+const CONFIG = require('../config/config')
 const multer = require('multer')
+
+const AWS = require('aws-sdk')
+var multerS3 = require('multer-s3')
+
+const s3 = new AWS.S3({
+  accessKeyId: CONFIG.AWS_ACCESS_KEY_ID,
+  secretAccessKey: CONFIG.AWS_SECRET_ACCESS_KEY,
+  signatureVersion: 'v4',
+  region: 'ap-south-1',
+})
 
 //TODO: Use S3 bucket.
 const upload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'public/foodListings')
+  storage: multerS3({
+    s3: s3,
+    acl: 'private',
+    bucket: CONFIG.AWS_S3_BUCKET_NAME,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname })
     },
-    filename: (req, file, cb) => {
-      const fileName = `${new Date().getTime()}_${file.originalname}`
-      file.key = `${process.env.BASE_URL}/foodListings/${fileName}`
-      cb(null, fileName)
+
+    key: (req, file, cb) => {
+      cb(null, new Date().getTime() + '_' + file.originalname)
     },
   }),
   limits: {
-    fileSize:
-      parseInt(process.env.MAX_IMAGE_UPLOAD_SIZE_MB || 30) * 1024 * 1024,
+    fileSize: parseInt(CONFIG.MAX_IMAGE_UPLOAD_SIZE_MB || 30) * 1024 * 1024,
   },
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
