8a148eb912465599f1bacdd57672f07cf440b5ae
Krishan-2000 <krishan.mishra2019@vitbhopal.ac.in>
2022-04-09 13:18:31 +0530

    added isActive filter in getAllDonation Api

--- controllers/foodListing.controller.js
+++ controllers/foodListing.controller.js
@@ -1,4 +1,8 @@
-const { FoodListing, validateCreate, validateId} = require('../models/foodListing.model')
+const {
+  FoodListing,
+  validateCreate,
+  validateId,
+} = require('../models/foodListing.model')
 const mongoose = require('mongoose')
 const add = async (req, res) => {
   const { error } = validateCreate(req.body)
@@ -20,49 +24,50 @@ const add = async (req, res) => {
     return res.status(500).send({ error: e.message })
   }
 }
-const deactivate = async (req,res)=>{
-    const {error}= validateId(req.body)
-    if (error) return res.status(400).send({ error: error.message })
-    try {
-      const donorID = req.uid;
-      const listingID= req.body.id;
-         const updatedFoodListing =await FoodListing.findOneAndUpdate({_id : listingID, donarId:donorID},{isActive : false} , {new : true})
-         if(!updatedFoodListing)
-         {
-            return res.status(404).send({
-              error: "Food listing not found"
-            })
-         }
-         return res.status(200).send({
-            message: 'Food listing successfully Deactivated.',
-            updatedFoodListing,
-         })
-    } catch (e) {
-      return res.status(500).send({ error: e.message })
+const deactivate = async (req, res) => {
+  const { error } = validateId(req.body)
+  if (error) return res.status(400).send({ error: error.message })
+  try {
+    const donorID = req.uid
+    const listingID = req.body.id
+    const updatedFoodListing = await FoodListing.findOneAndUpdate(
+      { _id: listingID, donarId: donorID },
+      { isActive: false },
+      { new: true }
+    )
+    if (!updatedFoodListing) {
+      return res.status(404).send({
+        error: 'Food listing not found',
+      })
     }
+    return res.status(200).send({
+      message: 'Food listing successfully Deactivated.',
+      updatedFoodListing,
+    })
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 }
-const getOne = async (req,res) =>{
-      if(!mongoose.isValidObjectId(req.params.id))
-      {
-          return res.status(500).send({
-            error: "Invalid Id"
-          });
-      }
-      try {
-        const food = await FoodListing.findById(req.params.id);
-        if(!food || !food.isActive)
-        {
-          return  res.status(404).send({
-            error: "Foodlisting not found"
-          });
-        }
-        return res.status(200).send({food});
-      } catch (e) {
-        return res.status(500).send({ error: e.message })
-      }
+const getOne = async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(500).send({
+      error: 'Invalid Id',
+    })
+  }
+  try {
+    const food = await FoodListing.findById(req.params.id)
+    if (!food || !food.isActive) {
+      return res.status(404).send({
+        error: 'Foodlisting not found',
+      })
+    }
+    return res.status(200).send({ food })
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 }
 const getAllDonations = async (req, res) => {
-  const fields = ['isVeg', 'typeOfDonor', 'quantity']
+  const fields = ['isVeg', 'typeOfDonor', 'quantity', 'isActive']
   const filterQuery = {}
   fields.forEach((fields) => {
     if (req.query[fields]) filterQuery[fields] = req.query[fields]
@@ -80,5 +85,5 @@ module.exports = {
   add,
   deactivate,
   getOne,
-  getAllDonations
+  getAllDonations,
 }
