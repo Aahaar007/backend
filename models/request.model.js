46d5000d58a0d0fbe9cfb9dfd509c33edb4deadd
Avijit Pandey <avijeetpandey87@gmail.com>
2022-04-15 14:30:17 +0530

    model: request: make code necessary in create validator

--- models/request.model.js
+++ models/request.model.js
@@ -34,7 +34,7 @@ const validateCreate = (data) => {
   const schema = Joi.object({
     orderId: Joi.objectId().required(),
     amount: Joi.number().min(1).required(),
-    code: Joi.string().length(12),
+    code: Joi.string().length(12).required(),
   })
   return schema.validate(data)
 }
